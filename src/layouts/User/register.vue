79be29459782b737b3d29ab992e5917bce02d264
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-15 11:25:19 +0500

    index js

---
+++ src/layouts/User/register.vue
@@ -0,0 +1,76 @@
+<template>
+  <Nav></Nav>
+  <section class="py-24 h-[100vh] flex">
+    <div class="conatainer w-full flex items-center justify-center">
+      <form class="max-w-sm mx-auto w-full">
+        <div class="mb-5">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Your email</label
+          >
+          <input
+            type="email"
+            v-model="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lime-500 focus:border-lime-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+            placeholder="admin@gmail.com"
+            required
+          />
+        </div>
+        <div class="mb-5">
+          <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Your password</label
+          >
+          <input
+            type="password"
+            v-model="password"
+            id="password"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lime-500 focus:border-lime-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+            placeholder="••••••••"
+            required
+          />
+        </div>
+        <button
+          type="button"
+          @click="login"
+          class="text-white bg-lime-500 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </section>
+</template>
+<script setup>
+import { ref } from "vue";
+import router from "@/router";
+import Nav from "../../components/Nav/UserNav.vue"
+
+const email = ref("");
+const password = ref("");
+
+function login() {
+  if (email.value == "admin@gmail.com" && password.value == "11111111") {
+    localStorage.setItem("token", "12345");
+    router.push("/");
+  } else {
+    alert("login failed");
+  }
+}
+</script>
+<style>
+section {
+  background-image: url("https://bogatyr.club/uploads/posts/2023-03/1679143074_bogatyr-club-p-fon-kriptovalyuta-foni-instagram-55.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: right;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Applayout from '../layouts/AppLayout/Main.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AppLayout from "../layouts/AppLayout/Main.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      component: Applayout,
+      path: "/",
+      component: AppLayout,
       children: [
         {
-          path: '/home',
-          name: 'home',
-          component: HomeView
-        }
-      ]
+          path: "",
+          name: "home",
+          component: HomeView,
+        },
+      ],
     },
     {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../layouts/User/login.vue')
+      path: "/register",
+      name: "register",
+      component: () => import("../layouts/User/register.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: () => import("../layouts/User/login.vue"),
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ],
+});
 
-export default router
+export default router;
