b5f37524aae0d0fe43615d0814a39bcd60bb7ac9
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-15 10:03:22 +0500

    Birinchi commit

---
+++ src/layouts/AppLayout/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <Nav></Nav>
+    <Home></Home>
+</template>
+<script setup>
+import Nav from "../../components/Nav/Main.vue"
+import Home from '../../views/HomeView.vue'
+import router from '@/router';
+if(localStorage.getItem('token') === null){
+        router.push('/login');
+    }
+</script>
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import router from '@/router';
+</script>
+
+<template>
+  <section class="py-24">
+      <div class="conatainer">
+  </div>
+  </section>
+</template>
