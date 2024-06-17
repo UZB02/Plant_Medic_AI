fd721e4678d09b0c0a0aa68d4274082f416665dd
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-17 19:07:26 +0500

    page ikki tugma

--- src/components/Page2/Main.vue
+++ src/components/Page2/Main.vue
@@ -17,7 +17,7 @@
             <button
               class="text-white flex items-center justify-center gap-1 bg-lime-500 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-base px-4 py-2 text-center dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800"
             >
-              Get Started
+              Learn More
             </button>
           </div>
         </div>
