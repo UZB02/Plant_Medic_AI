c8bbe0563ca9ebae4fe5a475999cc3958e3b2132
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-18 10:45:46 +0500

    bg gif

---
+++ src/components/Page3/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="h-[90vh] flex items-center justify-center">
+      <div class="container">
+        <div class="left flex items-center justify-center">
+            <div class="card flex items-center justify-center">
+                <div class="content flex flex-col items-center justify-center gap-3">
+                    <h1 class="title font-bold text-6xl lg:text-8xl text-center">
+                        Our Team
+                    </h1>
+                    <p class="copy md:w-4/5 w-full">
+                        A simple and easy-to-use AI-based image classification system for
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+                        alias ipsum quam quibusdam, voluptatum suscipit, excepturi ut nemo
+                        rerum cupiditate maxime eveniet aperiam omnis optio blanditiis cum
+                        sapiente culpa. Fuga?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
+<script setup>
+</script>
+<style scoped>
+    section{
+  background-image: url("../../img/plantGif.gif");
+  background-repeat: no-repeat;
+  background-size: 500px;
+  background-position: right;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white; 
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 import Nav from "@/components/Nav/Main.vue";
 import Page1 from "@/components/Page1/Main.vue";
 import Page2 from "@/components/Page2/Main.vue";
+// import Page3 from "@/components/Page3/Main.vue";
 
 </script>
 
@@ -11,4 +12,5 @@ import Page2 from "@/components/Page2/Main.vue";
   <Nav></Nav>
   <Page1></Page1>
   <Page2></Page2>
+  <!-- <Page3></Page3> -->
 </template>
