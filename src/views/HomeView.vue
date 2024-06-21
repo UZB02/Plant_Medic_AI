2c757933214e79021f4c14905c5d278a6e80b910
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-21 10:01:12 +0500

    page 3 coommentga olindi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,13 @@ import { ref } from "vue";
 import Nav from "@/components/Nav/Main.vue";
 import Page1 from "@/components/Page1/Main.vue";
 import Page2 from "@/components/Page2/Main.vue";
-import Page3 from "@/components/Page3/Main.vue";
+// import Page3 from "@/components/Page3/Main.vue";
 
 </script>
 
 <template>
   <Nav></Nav>
-  <section class="felex flex-col items-center gap-5 justify-center">
-      <Page1></Page1>
+  <Page1></Page1>
   <Page2></Page2>
-  <Page3 class="mt-7 py-9"></Page3>
-  </section>
+  <!-- <Page3></Page3> -->
 </template>
